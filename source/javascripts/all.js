e911733e97c89869c58ac5d5d5353f07bce7732e
Eric Fagot <fagot.eric@gmail.com>
2016-03-29 19:30:15 +0200

    Scroll and Navbar animation

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,3 +1,16 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(document).ready(function(){
+
+  $(window).on("scroll", function(){
+    if ( $(this).scrollTop() > 80 ){
+      $(".navbar").css("background-color" , "rgba(0, 0, 0, 0.7");
+    } else {
+      $(".navbar").css("background-color" , "transparent");
+    }
+
+  });
+
+});
